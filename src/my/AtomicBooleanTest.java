dd1ad1b78eefdf502178091e71c2c221ce1072fd
yedongyu <yedongyu@deepwise.com>
2020-05-26 20:32:01 +0800

    CAS(乐观锁) compare and swap

---
+++ src/my/AtomicBooleanTest.java
@@ -0,0 +1,44 @@
+package my;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @program: jdk_study
+ * @description: https://www.jianshu.com/p/ae25eb3cfb5d
+ * @author: YeDongYu
+ * @create: 2020-05-26 20:26
+ */
+public class AtomicBooleanTest implements Runnable{
+    private volatile AtomicBoolean flag = new AtomicBoolean(true);
+
+    public static void main(String[] args) {
+        AtomicBooleanTest ast = new AtomicBooleanTest();
+        Thread thread1 = new Thread(ast);
+        Thread thread = new Thread(ast);
+        thread1.start();
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("thread:"+Thread.currentThread().getName()+";flag:"+flag.get());
+        if (flag.compareAndSet(true,false)){
+            System.out.println(Thread.currentThread().getName()+""+flag.get());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            flag.set(true);
+        }else{
+            System.out.println("重试机制thread:"+Thread.currentThread().getName()+";flag:"+flag.get());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            run();
+        }
+
+    }
+}
