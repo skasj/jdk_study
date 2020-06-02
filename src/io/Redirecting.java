5b3ac5039c2282c13b34d976975fd8ec20519cf3
yedongyu <yedongyu@deepwise.com>
2020-06-02 20:14:58 +0800

    标准io重定向

--- src/io/Redirecting.java
+++ src/io/Redirecting.java
@@ -1,25 +1,44 @@
 package io;//: io/Redirecting.java
 // Demonstrates standard I/O redirection.
-import java.io.*;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 public class Redirecting {
-  public static void main(String[] args)
-  throws IOException {
-    PrintStream console = System.out;
-    BufferedInputStream in = new BufferedInputStream(
-      new FileInputStream("Redirecting.java"));
-    PrintStream out = new PrintStream(
-      new BufferedOutputStream(
-        new FileOutputStream("test.out")));
-    System.setIn(in);
-    System.setOut(out);
-    System.setErr(out);
-    BufferedReader br = new BufferedReader(
-      new InputStreamReader(System.in));
-    String s;
-    while((s = br.readLine()) != null)
-      System.out.println(s);
-    out.close(); // Remember this!
-    System.setOut(console);
-  }
+
+    public static void main(String[] args)
+            throws IOException {
+        PrintStream console = System.out;
+        InputStream inputStream = System.in;
+        BufferedInputStream in = new BufferedInputStream(
+                new FileInputStream("src/io/Redirecting.java"));
+        PrintStream out = new PrintStream(
+                new BufferedOutputStream(
+                        new FileOutputStream("src/io/test.out")));
+        System.setIn(in);
+        System.setOut(out);
+        System.setErr(out);
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+        out.close(); // Remember this!
+        // 切换回控制台输入输出
+        System.setIn(inputStream);
+        System.setOut(console);
+        br = new BufferedReader(new InputStreamReader(inputStream));
+        while ((s = br.readLine()) != null && s.length() != 0) {
+            System.out.println(s);
+        }
+        out.close();
+    }
 } ///:~
