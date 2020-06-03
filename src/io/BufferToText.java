b0e706d726c5c6095c9343027a4d372e26d94909
yedongyu <yedongyu@deepwise.com>
2020-06-03 11:01:55 +0800

    读取byte[],并转换为字符串

---
+++ src/io/BufferToText.java
@@ -0,0 +1,62 @@
+package io;//: io/BufferToText.java
+// Converting text to and from ByteBuffers
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class BufferToText {
+
+    private static final int BSIZE = 1024;
+
+    public static void main(String[] args) throws Exception {
+        FileChannel fc =
+                new FileOutputStream("src/io/data2.txt").getChannel();
+        fc.write(ByteBuffer.wrap("Some text".getBytes()));
+        fc.close();
+        fc = new FileInputStream("src/io/data2.txt").getChannel();
+        ByteBuffer buff = ByteBuffer.allocate(BSIZE);
+        fc.read(buff);
+        buff.flip();
+        // Doesn't work:
+        System.out.println(buff.asCharBuffer());
+        // work;
+        System.out.println(StandardCharsets.UTF_8.decode(buff));
+        // Decode using this system's default Charset:
+        buff.rewind();
+        String encoding = System.getProperty("file.encoding");
+        System.out.println("Decoded using " + encoding + ": "
+                + Charset.forName(encoding).decode(buff));
+        // Or, we could encode with something that will print:
+        fc = new FileOutputStream("src/io/data2.txt").getChannel();
+        fc.write(ByteBuffer.wrap(
+                "Some text".getBytes(StandardCharsets.UTF_16BE)));
+        fc.close();
+        // Now try reading again:
+        fc = new FileInputStream("src/io/data2.txt").getChannel();
+        buff.clear();
+        fc.read(buff);
+        buff.flip();
+        System.out.println(buff.asCharBuffer());
+        // Use a CharBuffer to write through:
+        fc = new FileOutputStream("src/io/data2.txt").getChannel();
+        buff = ByteBuffer.allocate(24); // More than needed
+        buff.asCharBuffer().put("Some text");
+        fc.write(buff);
+        fc.close();
+        // Read and display:
+        fc = new FileInputStream("src/io/data2.txt").getChannel();
+        buff.clear();
+        fc.read(buff);
+        buff.flip();
+        System.out.println(buff.asCharBuffer());
+    }
+} /* Output:
+????
+Decoded using Cp1252: Some text
+Some text
+Some text
+*///:~
